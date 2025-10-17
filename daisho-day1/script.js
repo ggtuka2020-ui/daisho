5f58f8421f9c5731343af56007c7a6768b9d85be
ggtuka2020-ui <ggtuka2020@gmail.com>
2025-10-17 11:58:27 +0900

    1201

---
+++ daisho-day1/script.js
@@ -0,0 +1,13 @@
+const helloBtn=document.getElementById("helloBtn");
+const message=document.getElementById("message");
+
+helloBtn.addEventListener("click"()=>{
+    const hour=new Date().getHours();
+    let greeting="こんにちは";
+    if (hour < 12){
+        greeting="おはよう";
+    }else if (hour >=18){
+        greeting ="こんばんわ";
+    }
+    message.textContent=${greeting}GitHub Pagesの公開に成功しました;
+});
