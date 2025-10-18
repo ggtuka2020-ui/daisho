5a3b04bb6594d156e711d206930c337d6d457f5e
ggtuka2020-ui <ggtuka2020@gmail.com>
2025-10-18 09:44:34 +0900

    947

--- daisho-day1/script.js
+++ daisho-day1/script.js
@@ -2,7 +2,7 @@ const helloBtn=document.getElementById("helloBtn");
 const message=document.getElementById("message");
 
 helloBtn.addEventListener("click",()=>{
-    const hour = new Date().getHours();
+    const hour = newDate().getHours();
     let greeting = "こんにちは";
     if (hour<12){
         greeting = "おはよう";
